f7afd45e104aaa4626547c03ac22cb8302dd1cc1
Vasiliy <mazurboss2016@gmail.com>
2022-11-13 16:46:58 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Movies } from 'pages/Movies';
+import { Container, Header, Link } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Header>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/products">Movies</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Movies />} />
+      </Routes>
+    </Container>
   );
 };
